4deb31fd5281c158306b2597af930a995ea56a14
Matheus <matheus_lopes151@hotmail.com>
2025-03-13 20:48:58 -0300

    elemento movel

---
+++ src/components/pages/dashboard/resume/resume-content/controls.tsx
@@ -0,0 +1,63 @@
+import { Button } from "@/components/ui/button";
+import { Tooltip } from "@/components/ui/tooltip";
+import { useResumeDownload } from "@/hooks/use-resume-download";
+import { cn } from "@/lib/utils"
+import { Download, RotateCcw, ZoomIn, ZoomOut } from "lucide-react"
+import { useControls } from "react-zoom-pan-pinch"
+
+type TransformControlsProps = {
+    title: string;
+}
+
+export const TransformControls = ({ title }: TransformControlsProps) => {
+    const { zoomIn, zoomOut, centerView } = useControls();
+
+    const { handleDownloadResume, isLoading } = useResumeDownload(title);
+
+    const controls = [
+        {
+            icon: ZoomIn,
+            label: "Aumentar Zoom",
+            onClick: () => zoomIn(0.2),
+        },
+        {
+            icon: ZoomOut,
+            label: "Diminuir Zoom",
+            onClick: () => zoomOut(0.2),
+        },
+        {
+            icon: RotateCcw,
+            label: "Resetar posição",
+            onClick: () => centerView(0.5),
+        },
+        {
+            icon: Download,
+            label: "Baixar PDF",
+            onClick: () => handleDownloadResume(),
+            disabled: isLoading,
+        }
+    ];
+
+    return (
+        <div
+            className={cn(
+                "absolute left-1/2 -translate-x-1/2 bottom-4 z-10 bg-background border border-muted",
+                "py-3 px-4 rounded-full flex items-center gap-2"
+            )}
+        >
+            {controls.map((control) => (
+                <Tooltip key={control.label} content={control.label}>
+                    <Button
+                        variant="secondary"
+                        className="w-6 h-6 bg-transparent"
+                        size="icon"
+                        onClick={control.onClick}
+                        disabled={control.disabled}
+                    >
+                        <control.icon size={16} />
+                    </Button>
+                </Tooltip>
+            ))}
+        </div>
+    )
+}
---
+++ src/components/pages/dashboard/resume/resume-content/header.tsx
@@ -0,0 +1,75 @@
+import { Button } from "@/components/ui/button";
+import { Tooltip } from "@/components/ui/tooltip";
+import { Copy, Download, Home, Trash } from "lucide-react";
+import Link from "next/link";
+import { DeleteResumeDialog } from "./delete-resume-dialog";
+import { DuplicateResumeDialog } from "./duplicate-resume-dialog";
+import { useResumeDownload } from "@/hooks/use-resume-download";
+
+type NavigationHeaderProps = {
+    title: string;
+};
+
+export const NavigationHeader = ({ title }: NavigationHeaderProps) => {
+    const { handleDownloadResume, isLoading } = useResumeDownload(title);
+
+    return (
+        <header className="absolute w-full left-0 top-0 z-10 p-2 bg-background border-b border-muted flex items-center justify-between gap-2">
+            <div className="flex items-center gap-2">
+                <Tooltip content="Voltar ao painel">
+                    <Link href="/dashboard/resumes" passHref>
+                        <Button
+                            variant="secondary"
+                            className="w-8 h-8 bg-transparent"
+                            size="icon"
+                        >
+                            <Home size={18} />
+                        </Button>
+                    </Link>
+                </Tooltip>
+
+                <span className="text-muted-foreground">/</span>
+
+                <p className="text-lg font-title font-bold ml-1">{title}</p>
+            </div>
+
+            <div className="flex gap-1">
+                <DeleteResumeDialog>
+                    <Tooltip content="Deletar Currículo">
+                        <Button
+                            variant="secondary"
+                            className="w-8 h-8 bg-transparent"
+                            size="icon"
+                        >
+                            <Trash size={18} />
+                        </Button>
+                    </Tooltip>
+                </DeleteResumeDialog>
+
+                <DuplicateResumeDialog>
+                    <Tooltip content="Duplicar Currículo">
+                        <Button
+                            variant="secondary"
+                            className="w-8 h-8 bg-transparent"
+                            size="icon"
+                        >
+                            <Copy size={18} />
+                        </Button>
+                    </Tooltip>
+                </DuplicateResumeDialog>
+
+                <Tooltip content="Baixar PDF">
+                    <Button
+                        variant="secondary"
+                        className="w-8 h-8 bg-transparent"
+                        size="icon"
+                        onClick={handleDownloadResume}
+                        disabled={isLoading}
+                    >
+                        <Download size={18} />
+                    </Button>
+                </Tooltip>
+            </div>
+        </header>
+    );
+};
