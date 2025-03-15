a6404f869536eb69e06f2fd4db1c00e91c549428
Matheus <matheus_lopes151@hotmail.com>
2025-03-15 17:42:56 -0300

    renderizar conteudo

--- src/components/pages/dashboard/resume/resume-content/templates/index.tsx
+++ src/components/pages/dashboard/resume/resume-content/templates/index.tsx
@@ -5,33 +5,33 @@ import { Jynx } from "./jynx";
 import { Onix } from "./onix";
 
 export type BaseResumeProps = {
-    data: ResumeData;
+  data: ResumeData;
 };
 
 type ResumeTemplateProps = {
-    data: ResumeData;
+  data: ResumeData;
 };
 
 const templatesMap: Record<ResumeTemplates, React.FC<BaseResumeProps>> = {
-    ditto: Ditto,
-    eevee: Eevee,
-    jynx: Jynx,
-    onix: Onix,
+  ditto: Ditto,
+  eevee: Eevee,
+  jynx: Jynx,
+  onix: Onix,
 };
 
 export const ResumeTemplate = ({ data }: ResumeTemplateProps) => {
-    const template = data.structure.template;
+  const template = data.structure.template;
 
-    const Resume = useMemo(() => {
-        return templatesMap[template];
-    }, [template]);
+  const Resume = useMemo(() => {
+    return templatesMap[template];
+  }, [template]);
 
-    return (
-        <div
-            id="resume-content"
-            className="w-[210mm] min-h-[297mm] bg-white text-black font-arial [&_hr]:border-black"
-        >
-            <Resume data={data} />
-        </div>
-    );
+  return (
+    <div
+      id="resume-content"
+      className="w-[210mm] min-h-[297mm] bg-white text-black font-arial [&_hr]:border-black"
+    >
+      <Resume data={data} />
+    </div>
+  );
 };
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,55 @@
-import { clsx, type ClassValue } from "clsx"
+import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge"
+import colors from "tailwindcss/colors"
+import tailwindConfig from "../../tailwind.config"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+export const sectionIsEmpty = (
+  section: ResumeSections,
+  data: ResumeContentData
+) => {
+  switch (section) {
+    case "summary":
+      return data.summary === "" || data.summary === "<p></p>";
+    default:
+      return data[section].length === 0;
+  }
+};
+
+export const formatTailwindHTML = (
+  html: string,
+  structure: ResumeStructureData,
+) => {
+  const colorKey = structure.colorTheme as keyof typeof colors;
+
+  return `
+    <html>
+      <head>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+          tailwind.config = ${JSON.stringify(tailwindConfig)};
+          document.documentElement.style.setProperty(
+            "--resume-primary",
+            "${colors[colorKey][500]}"
+          );
+        </script>
+      </head>
+
+      <body>
+        ${html}
+      </body>
+    </html>
+  `
+}
+
+export const isValidJSON = (json: string) => {
+  try {
+    JSON.parse(json);
+    return true;
+  } catch (error) {
+    return false;
+  }
 }
