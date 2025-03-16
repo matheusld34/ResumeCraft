bc2177bbe566fcfb5a9d0996b41c26d7210b60e8
Matheus <matheus_lopes151@hotmail.com>
2025-03-16 13:12:30 -0300

    auth.js

--- src/app/dashboard/(layout)/layout.tsx
+++ src/app/dashboard/(layout)/layout.tsx
@@ -1,27 +1,32 @@
 import { ReactNode } from "react"
 import Logo from "@/assets/logo.svg"
-import { NavItems } from "@/components/pages/dashboard/nav-items"
-import { UserDropdown } from "@/components/pages/dashboard/user-dropdown"
-import { ThemeToggle } from "@/components/shared/theme-toggle"
+import { NavItems } from "@/components/pages/dashboard/nav-items";
+import { UserDropdown } from "@/components/pages/dashboard/user-dropdown";
+import { ThemeToggle } from "@/components/shared/theme-toggle";
+import { auth } from "@/lib/auth";
 
 type DashboardLayoutProps = {
-    children: ReactNode
+    children: ReactNode;
 }
 
+export default async function DashboardLayout({ children }: DashboardLayoutProps) {
+    const session = await auth();
 
-export default function DashboardLayout({ children }: DashboardLayoutProps) {
     return (
         <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fr]">
             <aside className="w-full h-full flex flex-col items-center border-r border-muted">
-                <div className=" w-full p-6 border-b border-muted">
+                <div className="w-full p-6 border-b border-muted">
                     <Logo className="max-w-[100px] mx-auto" />
                 </div>
+
                 <NavItems />
+
                 <div className="w-full mt-auto border-t border-muted px-3 py-4 flex items-center justify-between gap-2">
-                    <UserDropdown />
+                    <UserDropdown user={session?.user} />
                     <ThemeToggle />
                 </div>
             </aside>
+
             <main className="p-6 flex flex-col w-full h-full overflow-auto">
                 {children}
             </main>
--- src/components/pages/dashboard/user-dropdown.tsx
+++ src/components/pages/dashboard/user-dropdown.tsx
@@ -1,43 +1,61 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
 import {
     DropdownMenu,
     DropdownMenuContent,
     DropdownMenuItem,
-
     DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Button } from "@/components/ui/button"
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import Link from "next/link"
-import { LogOut, SquareUser } from "lucide-react"
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
+import { LogOut, SquareUser } from "lucide-react";
+import { User } from "next-auth";
+import { signOut } from "next-auth/react";
 
+type UserDropdownProps = {
+    user?: User;
+};
 
-export const UserDropdown = () => {
+export const UserDropdown = ({ user }: UserDropdownProps) => {
+    if (!user) return null;
+
+    const initials = user?.name
+        ?.split(" ")
+        ?.slice(0, 2)
+        .map((name) => name[0])
+        .join("");
+
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="ghost" className="w-full gap-2 justify-start px-2">
                     <Avatar className="w-7 h-7 block">
-                        <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
+                        <AvatarImage src={user.image ?? ""} />
+                        <AvatarFallback>{initials}</AvatarFallback>
                     </Avatar>
-
-                    <p>Matheus Santos</p>
+                    <p>{user.name}</p>
                 </Button>
             </DropdownMenuTrigger>
-            <DropdownMenuContent align="center" className="w-[var(--radix-dropdown-menu-trigger-width)]">
+            <DropdownMenuContent
+                align="center"
+                className="w-[var(--radix-dropdown-menu-trigger-width)]"
+            >
                 <Link passHref href="/dashboard/account">
                     <DropdownMenuItem className="gap-2">
                         <SquareUser size={16} />
-                        Configurações de conta
+                        Configurações de Conta
                     </DropdownMenuItem>
                 </Link>
-                <DropdownMenuItem className="gap-2 text-red-500">
-                    <LogOut size={16} />
-                    Configurações de conta
-                </DropdownMenuItem>
 
+                <DropdownMenuItem
+                    className="gap-2 text-red-500"
+                    onClick={() => signOut({ callbackUrl: "/auth/login" })}
+                >
+                    <LogOut size={16} />
+                    Sair
+                </DropdownMenuItem>
             </DropdownMenuContent>
         </DropdownMenu>
-
-    )
-}
+    );
+};
