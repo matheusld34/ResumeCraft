7a57300cf1cb96c24676c690f225679b1e6114ed
Matheus <matheus_lopes151@hotmail.com>
2025-03-19 20:43:58 -0300

    geração de pdf

--- src/components/ui/tooltip/index.tsx
+++ src/components/ui/tooltip/index.tsx
@@ -3,25 +3,23 @@ import {
     TooltipContent,
     TooltipProvider,
     TooltipTrigger,
-} from "./primitive"
+} from "./primitive";
 import { ReactNode } from "react";
 
 type TooltipProps = {
     children: ReactNode;
-    content: string | number | ReactNode
+    content: string | number | ReactNode;
 }
 
-
-export const Tooltip = ({ children, content }: TooltipProps) => {
+export const Tooltip = ({ children, content, ...props }: TooltipProps) => {
     return (
         <TooltipProvider>
             <TooltipRoot delayDuration={300}>
-                <TooltipTrigger asChild>{children}</TooltipTrigger>
+                <TooltipTrigger asChild {...props}>{children}</TooltipTrigger>
                 <TooltipContent>
                     <p>{content}</p>
                 </TooltipContent>
             </TooltipRoot>
         </TooltipProvider>
-
-    )
-}
+    );
+};
